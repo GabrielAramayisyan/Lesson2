ca52f54c6ce51d1b1e17ecb32580912c7dc10597
GabrielAramayisyan <71033991+GabrielAramayisyan@users.noreply.github.com>
2020-09-14 19:56:31 +0400

    Changed something in code

--- MyJS/Script.js
+++ MyJS/Script.js
@@ -1,7 +1,7 @@
-const  Math_PI = 3.14;
+const PI = Math_PI;
 let r = parseFloat(prompt("Enter your radius value to find the area of circle"));
-let S = Math_PI * (r * r);
+let S = PI * (r * r);
 alert (S);
 let r2 = parseFloat(prompt("Enter your radius value to find the perimeter of circle"));
-let P =  2 * Math_PI * r2;
+let P =  2 * PI * r2;
 alert (P);
