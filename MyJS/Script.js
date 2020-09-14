e58de93cef945268a436da0679ede6515305a6fa
GabrielAramayisyan <71033991+GabrielAramayisyan@users.noreply.github.com>
2020-09-14 12:44:05 +0400

    Pushed Index.html file and MyJS folder to Lesson2 repo

---
+++ MyJS/Script.js
@@ -0,0 +1,7 @@
+const  Math_PI = 3.14;
+let r = parseFloat(prompt("Enter your radius value to find the area of circle"));
+let S = Math_PI * (r * r);
+alert (S);
+let r2 = parseFloat(prompt("Enter your radius value to find the perimeter of circle"));
+let P =  2 * Math_PI * r2;
+alert (P);
